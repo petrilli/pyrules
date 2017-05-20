fee10f99d3c26b64b52f4952e363849b58edd93c
Christopher Petrilli <petrilli@amber.org>
2017-05-19 21:09:19 -0700

    Reduce support for Python 2.7 for now

--- setup.py
+++ setup.py
@@ -43,9 +43,6 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements
